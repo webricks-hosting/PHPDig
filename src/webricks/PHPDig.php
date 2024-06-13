3007ae78c499e7ef4d4dc9063005d531477f692f
Jürgen Aloy <aloy@yoonic.at>
2024-06-13 11:14:21 +0200

    Added trace option & better parsing.

--- src/webricks/PHPDig.php
+++ src/webricks/PHPDig.php
@@ -14,19 +14,58 @@ class PHPDig {
     
     const BASE_ARGS = ['dig', '+noall', '+answer'];
     
+    /**
+     * The dns server.
+     * @var string
+     */
     private $server = null;
+    
+    /**
+     * Trace option flag.
+     * @var bool
+     */
+    private $trace = false;
     
     public function query($query, $type = null) {
         $data = $this->exec([$query, $type]);
+        if($this->trace)
+            return $data;
+        
         return $this->parseOutput($data);
     }
     
+    /**
+     * Get the dns server.
+     * @return string
+     */
     public function getServer() {
         return $this->server;
     }
 
+    /**
+     * Set the dns server.
+     * @param string $server
+     * @return void
+     */
     public function setServer($server): void {
         $this->server = $server;
+    }
+    
+    /**
+     * Get the trace flag.
+     * @return bool
+     */
+    public function getTrace(): bool {
+        return $this->trace;
+    }
+
+    /**
+     * Set the trace flag.
+     * @param booelan $trace
+     * @return void
+     */
+    public function setTrace(bool $trace): void {
+        $this->trace = $trace;
     }
     
     protected function parseOutput($out) {
@@ -41,7 +80,12 @@ class PHPDig {
             if(strlen($ltrimed) <= 0)
                 continue;
             
-            $parts = preg_split('/\t+/', $line, 5);
+            if(substr($ltrimed, 0, 2) == ';;')
+                continue;
+            
+            $parts = preg_split('/[\s]+/', $line, 5);
+            if(count($parts) !== 5)
+                var_dump($line);
             $ret[] = array(
                 'name' => $parts[0], 'ttl' => $parts[1], 'class' => $parts[2], 'type' => $parts[3], 'record' => $parts[4]
             );
@@ -54,6 +98,9 @@ class PHPDig {
         $baseArgs = self::BASE_ARGS;
         if($this->server != null)
             $baseArgs[] = '@' . $this->server;
+        
+        if($this->trace)
+            $baseArgs[] = '+trace';
         
         $execArgs = array_merge($baseArgs, $args);
         $p = new Process($execArgs);
--- tests/PHPDigTest.php
+++ tests/PHPDigTest.php
@@ -37,6 +37,28 @@ final class PHPDigTest extends TestCase {
         $this->assertSame('2001:503:ba3e::2:30', $res[0]['record']);
     }
     
+    public function testDigServer(): void {
+        $dig = new PHPDig();
+        $dig->setServer('8.8.8.8');
+        $res = $dig->query('a.root-servers.net', RecordType::A);
+
+        $this->assertSame(1, $this->count());
+        $this->assertSame('a.root-servers.net.', $res[0]['name']);
+        $this->assertTrue(is_numeric($res[0]['ttl']));
+        $this->assertTrue(intval($res[0]['ttl']) > 0);
+        $this->assertSame('IN', $res[0]['class']);
+        $this->assertSame('A', $res[0]['type']);
+        $this->assertSame('198.41.0.4', $res[0]['record']);
+    }
+    
+    public function testDigTrace(): void {
+        $dig = new PHPDig();
+        $dig->setServer(chr(rand(97, 109)) . '.root-servers.net');
+        $dig->setTrace(true);
+        $res = $dig->query('webricks.net', RecordType::A);
+        $this->assertStringContainsString('93.189.31.42', $res);
+    }
+
     public function testDigNonexistant(): void {
         $dig = new PHPDig();
         $res = $dig->query('lalala_iam_not_real_1235236757847547.com', RecordType::A);
